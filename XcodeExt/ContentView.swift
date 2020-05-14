24d50605ca82231e259bd70772b10fe1a4ddb412
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2020-05-14 22:23:46 +0800

    Initial Commit

---
+++ XcodeExt/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  XcodeExt
+//
+//  Created by Kolyutsakul, Thongchai on 14/5/20.
+//  Copyright © 2020 Thongchai Kolyutsakul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
