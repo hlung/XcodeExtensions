9eea37df59c5bc0c03dde5d0353b8aa7f7c489c3
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2020-05-15 00:12:49 +0800

    Add code

--- ExpandSelection/SourceEditorCommand.swift
+++ ExpandSelection/SourceEditorCommand.swift
@@ -9,6 +9,11 @@
 import Foundation
 import XcodeKit
 
+// First thing first. Shorten these long names...
+typealias Range = XCSourceTextRange
+typealias Position = XCSourceTextPosition
+typealias Buffer = XCSourceTextBuffer
+
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
 
   func perform(with invocation: XCSourceEditorCommandInvocation,
@@ -17,30 +22,81 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     defer { completionHandler(nil) }
 
     // At least something is selected
-    guard let firstSelection = invocation.buffer.selections.firstObject as? XCSourceTextRange,
-      let lastSelection = invocation.buffer.selections.lastObject as? XCSourceTextRange else {
-        return
+    guard let selection = invocation.buffer.selections.firstObject as? Range else { return }
+//    print(invocation.buffer.string(in: firstSelection))
+
+    // Discard other selections in the buffer
+    invocation.buffer.selections.removeAllObjects()
+//    invocation.buffer.selections.add(Range(start: Position(0, 0), end: Position(0, 1)))
+//    invocation.buffer.selections.add(Range(start: Position(1, 0), end: Position(1, 1)))
+
+    // Get selectedString
+    let selectedString = invocation.buffer.string(in: selection)
+    guard !selectedString.isEmpty else { return }
+
+    print("selectedString:", selectedString)
+
+    // Find next selection of the same string
+    if let nextRange = invocation.buffer.range(of: selectedString, after: selection.end) {
+      // Add that next selection
+      print("nextRange:", nextRange)
+      invocation.buffer.selections.add(nextRange)
+    }
+    else {
+      print("nextRange: not found")
     }
 
-    // More than one line is selected
-    guard firstSelection.start.line < lastSelection.end.line else { return }
+    // Done!
 
-    // sort(invocation.buffer.lines, in: firstSelection.start.line...lastSelection.end.line, by: <)
-    sort(invocation.buffer.lines, in: firstSelection.start.line...lastSelection.end.line, by: isLessThanIgnoringLeadingWhitespacesAndTabs)
+    // expr in Range(start: Position(line: 0, column: 0), end: Position(line: 0, column: 1))
+//    sort(invocation.buffer.lines, in: firstSelection.start.line...lastSelection.end.line, by: <)
+//    sort(invocation.buffer.lines, in: firstSelection.start.line...lastSelection.end.line, by: isLessThanIgnoringLeadingWhitespacesAndTabs)
   }
 
+//  func sort(_ inputLines: NSMutableArray, in range: CountableClosedRange<Int>, by comparator: (String, String) -> Bool) {
+//    guard range.upperBound < inputLines.count, range.lowerBound >= 0 else { return }
+//    let lines = inputLines.compactMap { $0 as? String }
+//    let sorted = Array(lines[range]).sorted(by: comparator)
+//    for lineIndex in range {
+//      inputLines[lineIndex] = sorted[lineIndex - range.lowerBound]
+//    }
+//  }
 
-  func sort(_ inputLines: NSMutableArray, in range: CountableClosedRange<Int>, by comparator: (String, String) -> Bool) {
-    guard range.upperBound < inputLines.count, range.lowerBound >= 0 else { return }
-    let lines = inputLines.compactMap { $0 as? String }
-    let sorted = Array(lines[range]).sorted(by: comparator)
-    for lineIndex in range {
-      inputLines[lineIndex] = sorted[lineIndex - range.lowerBound]
+//  func isLessThanIgnoringLeadingWhitespacesAndTabs(_ lhs: String, _ rhs: String) -> Bool {
+//    return lhs.trimmingCharacters(in: .whitespaces) < rhs.trimmingCharacters(in: .whitespaces)
+//  }
+
+}
+
+extension Position {
+  init(_ line: Int, _ column: Int) {
+    self.init()
+    self.line = line
+    self.column = column
+  }
+}
+
+extension Buffer {
+  func string(in range: Range) -> String {
+    // Only support one line for now
+    let string = lines[range.start.line] as! String
+    let begin = string.index(string.startIndex, offsetBy: range.start.column)
+    let end = string.index(string.startIndex, offsetBy: range.end.column)
+    return String(string[begin..<end])
+  }
+
+  func range(of string: String, after startPosition: Position) -> Range? {
+    for line in startPosition.line...linesLastPosition.line {
+      let string = lines[line] as? String ?? ""
+      if let range = string.range(of: string) {
+        return Range(start: Position(line, range.lowerBound.utf16Offset(in: string)),
+                     end: Position(line, range.upperBound.utf16Offset(in: string)))
+      }
     }
+    return nil
   }
 
-  func isLessThanIgnoringLeadingWhitespacesAndTabs(_ lhs: String, _ rhs: String) -> Bool {
-    return lhs.trimmingCharacters(in: .whitespaces) < rhs.trimmingCharacters(in: .whitespaces)
+  var linesLastPosition: Position {
+    Position(max(0, lines.count - 1), (lines.lastObject as? String)?.count ?? 0)
   }
-
 }
--- XcodeExtTests/XcodeExtTests.swift
+++ XcodeExtTests/XcodeExtTests.swift
@@ -7,28 +7,29 @@
 //
 
 import XCTest
+//import XcodeKit
 @testable import XcodeExt
+@testable import ExpandSelection
 
 class XcodeExtTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  func test_buffer() throws {
+    let buffer = XCSourceTextBuffer()
+  }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//  func testPerformanceExample() throws {
+//    // This is an example of a performance test case.
+//    self.measure {
+//      // Put the code you want to measure the time of here.
+//    }
+//  }
 
 }
